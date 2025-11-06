03b22b0603e2d529bf9e277dfdfea1cb4176ef81
Flames <vibe-coder@flames.com>
2025-11-06 09:05:33 +0000

    Update project prj_od3sbltd - 2025-11-06T09:05:33.014Z

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Services from './components/Services';
-import Pricing from './components/Pricing';
 import OrderForm from './components/OrderForm';
-import Footer from './components/Footer';
 
 function App() {
   return (
@@ -13,10 +11,8 @@ function App() {
       <main>
         <Hero />
         <Services />
-        <Pricing />
         <OrderForm />
       </main>
-      <Footer />
     </div>
   );
 }
